352d95bcac18a1adf87fd070f6aabfbc07ae86c3
Jodie Thomson <thecurlypurpleshoe@gmail.com>
2022-10-30 01:17:50 +1000

    start of track code - adjust to class

--- js/main.js
+++ js/main.js
@@ -1,10 +1,32 @@
 "use strict";
 
+let trackFile;
+let road;
+let grass;
+let roadFinish;
+
+
+function preload(){//does this need to stay in main?
+trackFile = loadStrings("track.txt");
+road = loadImage("assets/images/road.png");
+grass = loadImage("assets/images/grass.png");
+roadFinish = loadImage("assets/images/roadFinishLine.png");
+
+}
+
+
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(750, 750);//adjusted this to fit - can adjust tile sizes
+  road.resize(tileSize, tileSize);
+  grass.resize(tileSize, tileSize);
+  roadFinish.resize(tileSize, tileSize);
+
+  drawTrack();
 }
 
 function draw() {
-  background(100, 104, 250);
+  background(80);
+  drawSprites();
 
 }
+
---
+++ js/track.js
@@ -0,0 +1,53 @@
+"use strict";
+//will use this sketch for the track
+
+let tileX = 25;
+let tileY = 25;
+let tileSize = 50;
+let startX;
+let startY;
+let arrayIndex1;
+let arrayIndex2;
+let grassGroup;
+let roadGroup;
+
+
+
+
+function drawTrack(){//convert to class
+  grassGroup = new Group();
+  roadGroup = new Group();
+  for(let i = 0; i < trackFile.length; i++){
+    let trackTypes = splitTokens(trackFile[i], ' ');
+
+    for (let z = 0; z<trackTypes.length; z++){
+      if(trackTypes[z] == 0){
+        trackTypes[z] = createSprite(tileX,tileY,tileSize,tileSize);
+        trackTypes[z].addImage(grass);
+        trackTypes[z].immovable = true;
+        grassGroup.add(trackTypes[z]);
+      }
+      else if (trackTypes[z] == 1){
+        trackTypes[z] = createSprite(tileX,tileY,tileSize,tileSize);
+        trackTypes[z].addImage(road);
+        trackTypes[z].immovable = true;
+        roadGroup.add(trackTypes[z]);
+      }
+      else if (trackTypes[z] == 2){
+        trackTypes[z] = createSprite(tileX,tileY,tileSize,tileSize);
+        trackTypes[z].addImage(roadFinish);
+        trackTypes[z].immovable = true;
+        startX = tileX;
+        startY = tileY;
+        arrayIndex1 = i;
+        arrayIndex2 = z;
+      }
+      tileX += tileSize;
+    }
+    tileX = 25;
+    tileY += tileSize;
+        
+      
+    
+  }
+}
